f811ee431370e41bd415bad989ac8ed56607d4ac
Marlon Leon <ghostveene@gmail.com>
2023-01-31 11:27:15 -0500

    getting a new array with the emotionstag

--- index.js
+++ index.js
@@ -171,10 +171,12 @@ const catsData = [
 
 function getEmotionsArray(cats){
     const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
-        }
+
+    for (let cat of cats) {
+        for (let emotions of cat.emotionTags) {
+            emotionsArray.push(emotions)
+        } 
     }
-console.log(emotionsArray)
 }
+
+getEmotionsArray(catsData)
